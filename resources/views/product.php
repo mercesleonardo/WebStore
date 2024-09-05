c9101ec90b1121b4c4171d3c7d20c0abc0ab8202
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-05 11:38:25 -0300

    Ajustando a page produtos

--- resources/views/product.php
+++ resources/views/product.php
@@ -3,17 +3,43 @@
 <?php require 'includes/header.php'; ?>
 
 <main class="mx-auto max-w-7xl py-6 px-8">
-    <?php if ($product): ?>
-        <h2 class="text-xl loading-6 font-bold text-gray-900"><?= $product->name?> - <?= format_money($product->price) ?></h2>
+    <div class="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+        <div class="flex w-full">
+            <img
+                src="<?= $product->image ?>"
+                title="<?= $product->name ?>"
+                alt="<?= $product->name ?>"
+                class="w-full h-full object-cover object-center rounded-lg shadow-lg">
+        </div>
+        <div class="mt-10 px-4">
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900">
+                <?= $product->name ?>
+            </h2>
 
-        <p class="mt-4 text-gray-600">
-            Description:
-            <br>
-            <small class="text-gray-700">
-                <?= $product->description ?>
-            </small>
-        </p>
-    <?php endif;?>
+            <div class="mt-3">
+                <p class="text-3xl tracking-tight text-gray-900">
+                    <?= format_money($product->price) ?>
+                </p>
+            </div>
+
+            <div class="mt-6">
+                <div class="space-y-6 text-base text-gray-700">
+                    <p><?= $product->description ?></p>
+                </div>
+            </div>
+
+            <div class="mt-6 flex">
+                <button class="
+                    max-w-xs px-4 py-2 rounded-md
+                    bg-gray-800 hover:bg-gray-700
+                    text-base text-white font-medium
+                    focus:ring-gray-800 focus:ring-2 focus:ring-offset-2
+                ">
+                    Add to Cart
+                </button>
+            </div>
+        </div>
+    </div>
 </main>
 
 <?php require 'includes/footer.php'; ?>
