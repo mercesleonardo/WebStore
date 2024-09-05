9b1410866de8f26cf445d1c7727f712901281c17
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-05 15:00:07 -0300

    Ajuste no product controller

--- src/Controllers/product.php
+++ src/Controllers/product.php
@@ -2,12 +2,10 @@
 
 declare(strict_types = 1);
 
-use Core\Containers\Container;
-use Core\Database\Connector;
-
 $db = \container('db');
 
-$product = $db->query('SELECT * FROM products where id = :id', ['id' => $_GET['id']])->first();
+$query = 'SELECT * FROM products where id = :id';
+$product = $db->query($query, ['id' => $_GET['id']])->first();
 
 $title = $product->name . ' | My WebStore';
 $heading = 'Product Details';
