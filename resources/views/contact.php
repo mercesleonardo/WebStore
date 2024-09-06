9275cd9b9524d9e0943bf7c2e70f4c6b05e83fd9
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-05 23:52:28 -0300

    Formulário adicionado em contact.php

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -4,7 +4,47 @@
 
     <main>
         <div class="mx-auto max-w-7xl py-6 px-8">
-            Contact content
+
+<!--            --><?php //if ($messageWasSent) : ?>
+<!--                <div class="mb-4 px-4 py-4 w-full rounded-lg bg-green-600 text-green-900 font-bold">-->
+<!--                    Sua mensagem foi enviada com sucesso!-->
+<!--                </div>-->
+<!--            --><?php //endif ?>
+
+            <form method="post">
+                <div class="mb-4">
+                    <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                    <input type="text" id="name" name="name" class="mt-1 block w-full form-input py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                </div>
+
+                <div class="mb-4">
+                    <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                    <input type="email" id="email" name="email" class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                </div>
+
+                <div class="mb-4">
+                    <label for="source" class="block text-sm font-medium text-gray-700">Where did you know us?</label>
+                    <select name="source" id="source" class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        <option value="google">Google</option>
+                        <option value="facebook">Facebook</option>
+                        <option value="twitter">Twitter</option>
+                        <option value="instagram">Instagram</option>
+                    </select>
+                </div>
+
+                <div class="mb-4">
+                    <label for="message" class="block text-sm font-medium text-gray-700">Message</label>
+                    <textarea id="message" name="message" class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"></textarea>
+                </div>
+
+                <button type="reset" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                    Reset
+                </button>
+
+                <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Submit
+                </button>
+            </form>
         </div>
     </main>
 
