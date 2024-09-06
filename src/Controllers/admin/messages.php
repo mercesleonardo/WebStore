49d8b9bf1e382148275ed15bba578244b11fd700
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-06 11:11:56 -0300

    Table criado para vizualização das mensagens

--- src/Controllers/admin/messages.php
+++ src/Controllers/admin/messages.php
@@ -5,10 +5,10 @@ declare(strict_types = 1);
 use Core\Database\Connector;
 
 $title = 'My Messages';
-$heading = 'Home';
+$heading = $title;
 
 $db = \container(Connector::class);
 
-$products = $db->query('SELECT * FROM messages')->get();
+$messages = $db->query('SELECT * FROM messages')->get();
 
 require resource_path('views/admin/messages.php');
