cb9bed6b20227821dc0ad39f1ba936da1a14d425
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-10 10:09:49 -0300

    Removes FlashOldData

--- Core/Middlewares/Middleware.php
+++ Core/Middlewares/Middleware.php
@@ -6,7 +6,6 @@ class Middleware
 {
     protected static array $globalMiddlewares = [
         StartSession::class,
-        FlashOldData::class
     ];
 
     public static function getGlobalMiddlewares(): array
