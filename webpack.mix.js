fed1878c36a107d1ca74135d022fd142a2c9f798
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-11 11:48:00 -0300

    Adds option to compile apine

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,8 @@ const mix = require('laravel-mix');
 mix.setPublicPath('public');
 mix.version();
 
-mix.postCss('resources/css/app.css', 'public/css', [
-    require('tailwindcss'),
-]);
+mix.js('resources/js/app.js', 'public/js/app.js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+    ]);
+
