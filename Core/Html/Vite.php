0cc4019fb1725e46c11ab8bb394f1f0fea9c8249
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-11 23:18:21 -0300

    Adds `isCss` methods

--- Core/Html/Vite.php
+++ Core/Html/Vite.php
@@ -16,14 +16,37 @@ class Vite
         return is_file($this->hotFile);
     }
 
-    public function __invoke(string | array $entrypoints)
+    public function __invoke(string|array $entrypoints): string
     {
         if (is_string($entrypoints)) {
             $entrypoints = [$entrypoints];
         }
 
         if ($this->isRunningHot()) {
+            array_unshift($entrypoints, '@vite/client');
 
+            return $this->hot($entrypoints);
         }
+
+        return '';
+    }
+
+    private function hot(array $entrypoints)
+    {
+    }
+
+    private function makeTagForChunk(string $path)
+    {
+
+    }
+
+    private function hotAsset(string $entrypoint): string
+    {
+        return rtrim(file_get_contents($this->hotFile())) . '/' . $entrypoint;
+    }
+
+    private function isCss(string $path): bool
+    {
+        return preg_match('/\.(css|less|sass|scss|styl|stylus|pcss|postcss)$/', $path) === 1;
     }
 }
