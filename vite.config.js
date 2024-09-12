422f80b0a80a2bfeb84be4924ea8f7599254b99f
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-11 22:52:38 -0300

    Adjusts in vite.js and package.json

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel([
-            'resource/css/app.css',
-            'resource/js/app.js',
+            'resources/css/app.css',
+            'resources/js/app.js',
         ])
     ]
 });
