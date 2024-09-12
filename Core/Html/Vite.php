9c20e7b57937fe833087f4e7176bd07279d97ed2
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-11 23:35:57 -0300

    Vite.php has been configured

--- Core/Html/Vite.php
+++ Core/Html/Vite.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Core\Html;
 
@@ -13,10 +13,10 @@ class Vite
 
     public function isRunningHot(): bool
     {
-        return is_file($this->hotFile);
+        return is_file($this->hotFile());
     }
 
-    public function __invoke(string|array $entrypoints): string
+    public function __invoke(string | array $entrypoints): string
     {
         if (is_string($entrypoints)) {
             $entrypoints = [$entrypoints];
@@ -31,13 +31,20 @@ class Vite
         return '';
     }
 
-    private function hot(array $entrypoints)
+    private function hot(array $entrypoints): string
     {
+        $entrypoints = array_map(fn ($entrypoint) => $this->makeTagForChunk($this->hotAsset($entrypoint)), $entrypoints);
+
+        return implode(PHP_EOL, $entrypoints);
     }
 
-    private function makeTagForChunk(string $path)
+    private function makeTagForChunk(string $path): string
     {
+        if ($this->isCss($path)) {
+            return $this->makeCssTag($path);
+        }
 
+        return $this->makeJavascriptTag($path);
     }
 
     private function hotAsset(string $entrypoint): string
@@ -48,5 +55,32 @@ class Vite
     private function isCss(string $path): bool
     {
         return preg_match('/\.(css|less|sass|scss|styl|stylus|pcss|postcss)$/', $path) === 1;
+    }
+
+    private function makeCssTag(string $path): string
+    {
+        $attributes = $this->parseAttributes([
+            'rel'  => 'stylesheet',
+            'href' => $path,
+        ]);
+
+        return '<link ' . implode(' ', $attributes) . ' />';
+    }
+
+    private function makeJavascriptTag(string $path): string
+    {
+        $attributes = $this->parseAttributes([
+            'type' => 'module',
+            'src'  => $path,
+        ]);
+
+        return '<script ' . implode(' ', $attributes) . '></script>';
+    }
+
+    private function parseAttributes(array $attributes): array
+    {
+        return array_map(function ($key, $value) {
+            return $key . '="' . $value . '"';
+        }, array_keys($attributes), $attributes);
     }
 }
