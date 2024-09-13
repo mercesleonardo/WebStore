6dc9792a52be7c0c068dc927f98c8aaad09d33d1
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-13 11:12:52 -0300

    Adds `login` template

---
+++ resources/views/templates/login.php
@@ -0,0 +1,5 @@
+<?php include resource_path('views/includes/head.php') ?>
+    <main class="flex min-h-full flex-1 flex-col justify-centerpx-6 py-12 lg:px8">
+        <?php include $view ?>
+    </main>
+<?php include resource_path('views/includes/footer.php') ?>
