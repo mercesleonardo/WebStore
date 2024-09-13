405160e0f244b4b386fbf9e67652de1245ea41b1
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-13 11:08:41 -0300

    Controllers `create.php` has been up set

--- src/Controllers/contact/create.php
+++ src/Controllers/contact/create.php
@@ -2,19 +2,21 @@
 
 declare(strict_types = 1);
 
+use Core\Html\View;
 use Core\Session\Session;
 
 /** @var Session $session */
 $session = container(Session::class);
 
-$title   = 'Contact Us';
-$heading = $title;
-
-$success = $session->getFlash('success');
-$error   = $session->getFlash('danger');
-
 $sources = [
-    'google', 'facebook', 'twitter', 'instagram'
+    'google', 'facebook', 'twitter', 'instagram',
 ];
 
-require view('contact.php');
+echo (new View())
+    ->render('contact', [
+        'title'   => 'Contact Us',
+        'heading' => 'Contact Us',
+        'success' => $session->getFlash('success'),
+        'error'   => $session->getFlash('danger'),
+        'sources' => $sources,
+    ]);
