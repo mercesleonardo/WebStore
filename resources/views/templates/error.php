44441bd406f2aa0c5dbd13dcb60fa79b8d510507
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-13 11:25:58 -0300

    error.php has been created

---
+++ resources/views/templates/error.php
@@ -0,0 +1,10 @@
+<?php include resource_path('views/includes/head.php') ?>
+<?php include resource_path('views/includes/top-nav.php') ?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 px-8">
+        <?php include $view ?>
+    </div>
+</main>
+
+<?php include resource_path('views/includes/footer.php') ?>
