14ec628b0934ac43d8f0aa912050e952e86ed12e
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-13 11:04:40 -0300

    `default.php` has been up set

--- resources/views/templates/default.php
+++ resources/views/templates/default.php
@@ -1,1 +1,9 @@
-<?php
+<?php include resource_path('views/includes/head.php') ?>
+<?php include resource_path('views/includes/top-nav.php') ?>
+<?php include resource_path('views/includes/header.php') ?>
+
+<main class="mx-auto max-w-7xl py-6 px-8">
+    <?php include $view ?>
+</main>
+
+<?php include resource_path('views/includes/footer.php') ?>
