331585bff7c25db9a1693fd7ebc3a2eb5a2b5a01
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-22 20:43:55 -0300

    Adds option to show toasts that comes from flash

--- resources/views/includes/footer.php
+++ resources/views/includes/footer.php
@@ -4,5 +4,14 @@
             <div class="flex w-full flex-col items-center space-y-4 sm:items-end" id="notifications"></div>
         </div>
 
+        <script>
+            window.onload = () => {
+                <?php /** @var App\Helpers\Toast|null $toast */ ?>
+                <?php if ($toast = Core\Facades\Session::getFlash('toast')): ?>
+                window.toast('<?= $toast->type ?>', '<?= $toast->message ?>');
+                <?php endif; ?>
+            }
+        </script>
+
     </body>
 </html>
