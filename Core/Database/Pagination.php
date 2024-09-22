15ce8fab03aff10809c82416d8d852a84cf9caef
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-21 21:32:01 -0300

    Pagination class created

--- Core/Database/Pagination.php
+++ Core/Database/Pagination.php
@@ -1,8 +1,27 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Core\Database;
+
+use Illuminate\Support\Collection;
 
 class Pagination
 {
+    protected string $pageName = 'page';
 
+    protected int $onEachSide = 3;
+
+    protected int $lastPage;
+
+    public function __construct(
+        protected Collection $items,
+        protected int $total,
+        protected int $perPage,
+        protected int $currentPage,
+        protected string $path = '/',
+    ) {
+        $this->lastPage = max((int) ceil($this->total / $this->perPage), 1);
+        $this->path     = $this->path !== '/' ? rtrim($this->path, '/') : $this->path;
+    }
 }
