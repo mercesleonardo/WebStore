35e810df1c2d1d28e7fbaf1efe9808deab32a208
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-22 19:58:03 -0300

    Adds div to toast

--- resources/views/includes/footer.php
+++ resources/views/includes/footer.php
@@ -1,3 +1,8 @@
         </div>
+
+        <div class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6">
+            <div class="flex w-full flex-col items-center space-y-4 sm:items-end" id="notifications"></div>
+        </div>
+
     </body>
 </html>
