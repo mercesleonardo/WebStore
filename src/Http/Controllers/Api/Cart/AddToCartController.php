85f91d6c10ccbb6ce720ede9e9142fc37aea4678
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-09-23 10:55:11 -0300

    style issue fixed

--- src/Http/Controllers/Api/Cart/AddToCartController.php
+++ src/Http/Controllers/Api/Cart/AddToCartController.php
@@ -15,7 +15,7 @@ class AddToCartController
     #[Route('/api/cart/add', HttpMethod::Post)]
     public function __invoke(Request $request)
     {
-        $product = Product::find($request->get('id'));
+        $product      = Product::find($request->get('id'));
         $previousCart = collect(Session::get('cart', []));
 
         if ($previousCart->contains('id', $product->id)) {
@@ -28,10 +28,10 @@ class AddToCartController
             });
         } else {
             $cart = $previousCart->push([
-                'id' => $product->id,
-                'name' => $product->name,
-                'price' => $product->price,
-                'image' => $product->image,
+                'id'       => $product->id,
+                'name'     => $product->name,
+                'price'    => $product->price,
+                'image'    => $product->image,
                 'quantity' => 1,
             ]);
         }
