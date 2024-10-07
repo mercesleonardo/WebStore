8adf3a44d23b7194df1c35105a6f4e22d5997a56
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-10-07 11:02:14 -0300

    Add method to genarate a regex for each route and improve `match` method

--- Core/Router/Route.php
+++ Core/Router/Route.php
@@ -8,22 +8,34 @@ use Core\Http\Enums\HttpMethod;
 
 readonly class Route
 {
+    private string $regex;
+
     public function __construct(
         public string $uri,
         public string $controller,
         public HttpMethod $method,
         public string $action,
         public array $middlewares = []
-    ) {}
+    ) {
+        $this->genarateRegex();
+    }
 
     public function match(string $uri, string $method): bool
     {
-        return $this->uri === $uri && $this->method->value === $method;
+        return preg_match($this->regex, $uri) === 1 && $this->method->value === $method;
     }
 
     public function run(): mixed
     {
         return container()
             ->call($this->controller, $this->action);
+    }
+
+    private function genarateRegex(): void
+    {
+        $uri = preg_replace('/\{([a-zA-Z0-9]+)\}/', '?(/[a-zA-Z0-9\-_]+)+', $this->uri);
+        $uri = str_replace('/', '\/', $uri);
+
+        $this->regex = '/^' . $uri . '$/';
     }
 }
